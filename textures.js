8a78596e822a6488fffe0a3026599b2b61827d05
SystemAdmi <78592780+SystemAdmi@users.noreply.github.com>
2023-08-04 19:17:29 +0300

    Update textures.js

--- textures.js
+++ textures.js
@@ -1,8 +1,16 @@
-const passwords = {
-    'FreeScript': false,
-    'Trap2808': true
+const MAIN = {
+    ms: 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
 }
 
 const script = false
 
-!script ? document.pkgNotif('Script off') : console.warn('Script load')
+const examination = () => {
+    function action() {
+        document.loader(MAIN.ms, 'Script off')
+        document.pkgNotif('Script off')
+    }
+
+    action()
+}
+
+!script ? examination() : console.warn('Script load')
