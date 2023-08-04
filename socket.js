0b371a216b14ba88b07cb0e634e31f0e53a59417
SystemAdmi <78592780+SystemAdmi@users.noreply.github.com>
2023-08-04 17:20:45 +0300

    Create socket.js

---
+++ socket.js
@@ -0,0 +1,15 @@
+class Menu {
+    constructor(width, height, borderRadius) {
+        this.width = width
+        this.height = height
+        this.borderRadius = borderRadius
+    }
+}
+
+/*
+const menuSettings = new Menu({
+    width: 100,
+    height: 100,
+    borderRadius: 30,
+})
+*/
